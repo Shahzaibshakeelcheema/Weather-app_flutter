9636e0d59a6dabc11b5cc7b98cc609752667ad26
Shahzaibshakeelcheema <Shahzaibshakeelcheema@gmail.com>
2024-11-20 19:46:33 +0500

    Loading screen updated

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,8 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class Loading extends StatelessWidget {
+class Loading extends StatefulWidget {
   const Loading({super.key});
 
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+final LocationSettings locationSettings = LocationSettings(
+  accuracy: LocationAccuracy.high,
+  distanceFilter: 100,
+);
+
+Future <void> getLocation () async {
+
+  PermissionStatus permission =  await Permission.location.request();
+
+  if (permission == PermissionStatus.granted) {
+    try{
+
+      Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+
+      print ("user's location: ${position.latitude}, ${position.longitude}");
+
+    }
+    catch(e){
+      print("Error fetching the location: $e");
+    }
+  }
+  else if (permission== PermissionStatus.denied)
+{
+  print("Location permission denied");
+}
+else if (permission== PermissionStatus.permanentlyDenied)
+{
+print ('Location permission permanently denied');
+openAppSettings();
+
+}}
+
+class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
